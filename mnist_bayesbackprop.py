9e96162f3752d2bb9bb0e4fa73f80eaa20fbfd61
Daeil Kim <daeil.kim@gm.slc.edu>
2016-03-23 18:33:35 -0400

    Update mnist_bayesbackprop.py

--- mnist_bayesbackprop.py
+++ mnist_bayesbackprop.py
@@ -1,56 +1,125 @@
+'''
+Author: Daeil Kim
+Code adapted from Jan Hendrik Metzen using variational autoencoders: https://jmetzen.github.io/2015-11-27/vae.html
+Tensorflow Implementation for Bayes by BackProp - http://arxiv.org/pdf/1505.05424v2.pdf
+'''
+
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
 
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+def xavier_init(fan_in, fan_out, constant=1):
+    """ Xavier initialization of network weights"""
+    # https://stackoverflow.com/questions/33640581/how-to-do-xavier-initialization-on-tensorflow
+    low = -constant*np.sqrt(6.0/(fan_in + fan_out))
+    high = constant*np.sqrt(6.0/(fan_in + fan_out))
+    return tf.random_uniform((fan_in, fan_out),
+                             minval=low, maxval=high,
+                             dtype=tf.float32)
 
-graph = tf.Graph()
-num_features = 784 # 28x28 pixels
-num_labels = 10 # training
-num_steps = 1000
-batch_size = 100
-num_units = 400
+
+def train(network_architecture, training_epochs=10, batch_size=100, learning_rate=1e-3, display_step=1):
+    # Training cycle
+    bayes_backprop = BayesBackprop(network_architecture,
+                             learning_rate=learning_rate,
+                             batch_size=batch_size)
+
+    for epoch in range(training_epochs):
+        avg_cost = 0.
+        total_batch = int(n_samples / batch_size)
+
+        # Loop over all batches
+        for i in range(total_batch):
+            batch_xs, batch_labels = mnist.train.next_batch(batch_size)
+
+            # Fit training using batch data
+            cost = bayes_backprop.partial_fit(batch_xs, batch_labels)
+
+            # Compute average loss
+            avg_cost += cost / n_samples * batch_size
+
+        # Display logs per epoch step
+        if epoch % display_step == 0:
+            feed = {bayes_backprop.x: mnist.test.images,
+                    bayes_backprop.y_: mnist.test.labels}
+            print "Epoch:", '%04d' % (epoch+1), \
+                  "cost = ","{:.9f}".format(avg_cost), \
+                  "Accuracy: %2.3f" % (bayes_backprop.accuracy.eval(feed_dict=feed)*100)
+
+    return bayes_backprop
+
+
+'''
+Class function that initializes the BayesBackprop Deep Network
+'''
+
+
+class BayesBackprop():
+    def __init__(self, network_architecture, batch_size, learning_rate=0.01):
+        self.network_architecture = network_architecture
+        self.transfer_fct = tf.nn.softplus
+        self.learning_rate = learning_rate
+
+        self.x = tf.placeholder(tf.float32, [None, network_architecture["n_features"]])
+        self.y_ = tf.placeholder(tf.float32, [None, network_architecture["n_labels"]])
+
+        weights, biases = self._initialize_variables(self.network_architecture)
+        self._create_network(weights, biases)
+        self._create_loss_optimizer()
+
+        # Initialize variables and start model
+        init = tf.initialize_all_variables()
+        self.sess = tf.InteractiveSession()
+        self.sess.run(init)
+
+    def _create_network(self, weights, biases):
+        # Initialize network weights and biases
+        layer_1 = self.transfer_fct(tf.add(tf.matmul(self.x, weights['h1']), biases['b1']))
+        layer_2 = self.transfer_fct(tf.add(tf.matmul(layer_1, weights['h2']), biases['b2']))
+        self.y = tf.add(tf.matmul(layer_2, weights['h3_out']), biases['b3_out'])
+
+    def _initialize_variables(self, network_architecture):
+        n_features = network_architecture['n_features']
+        n_hidden_units = network_architecture['n_hidden_units']
+        n_labels = network_architecture['n_labels']
+        weights = {
+            'h1': tf.Variable(xavier_init(n_features, n_hidden_units)),
+            'h2': tf.Variable(xavier_init(n_hidden_units, n_hidden_units)),
+            'h3_out': tf.Variable(xavier_init(n_hidden_units, n_labels))}
+
+        biases = {
+            'b1': tf.Variable(tf.zeros([n_hidden_units], dtype=tf.float32)),
+            'b2': tf.Variable(tf.zeros([n_hidden_units], dtype=tf.float32)),
+            'b3_out': tf.Variable(tf.zeros([n_labels], dtype=tf.float32))}
+        return weights, biases
+
+    def _create_loss_optimizer(self):
+        self.correct_prediction = tf.equal(tf.argmax(self.y, 1), tf.argmax(self.y_, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
+        self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(self.y, self.y_))
+        # Use ADAM optimizer
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.cost)
+
+    def partial_fit(self, batch_xs, batch_labels):
+        """Train model based on mini-batch of input data.
+
+        Return cost of mini-batch.
+        """
+        opt, cost = self.sess.run((self.optimizer, self.cost),
+                                  feed_dict={self.x: batch_xs, self.y_: batch_labels})
+        return cost
+
 
 if __name__ == "__main__":
     # Initialize the tensorflow graph
-    graph = tf.Graph()
-    train_subset = 200
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+    n_samples = mnist.train.num_examples
 
-    with graph.as_default():
-        x = tf.placeholder(tf.float32, [None, num_features], name="x-input")
-        y_ = tf.placeholder(tf.float32, [None, num_labels], name='y-input')
+    network_architecture = \
+        dict(n_hidden_units=500, # Number of hidden layer ReLu neurons
+             n_features=784, # MNIST data input (img shape: 28*28)
+             n_labels=10)  # number of class labels
 
-        # Calculate weights and biases at the first layer given mu and sigma
-        bias1 = tf.Variable(tf.constant(0.1, shape=[num_units]), name="bias1")
-        bias2= tf.Variable(tf.constant(0.1, shape=[num_units]), name="bias2")
-        bias_out = tf.Variable(tf.constant(0.1, shape=[num_labels]), name="bias_out")
-        weights1 = tf.Variable(tf.random_normal([num_features, num_units]), name="weights_layer1")
-        weights2 = tf.Variable(tf.random_normal([num_units, num_units]), name="weights_layer2")
-        weights_out = tf.Variable(tf.random_normal([num_units, num_labels]), name="weights_out")
+    vae = train(network_architecture, training_epochs=20)
 
-        # Rectified Linear Unit Layers
-        h_relu1 = tf.nn.relu(tf.add(tf.matmul(x, weights1), bias1))
-        h_relu2 = tf.nn.relu(tf.add(tf.matmul(h_relu1, weights2), bias2))
-        y = tf.matmul(h_relu2, weights_out) + bias_out
 
-        # Specify the cost function. We'll want to replace this with the BayesBackprop cost
-        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
-        _ = tf.scalar_summary('cross entropy', cost)
-
-        # Specify the optimizer
-        optimizer = tf.train.AdamOptimizer(1e-2).minimize(cost)
-
-        # Calculate accuracy
-        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-    with tf.Session(graph=graph) as session:
-        tf.initialize_all_variables().run()
-        for step in range(num_steps):
-            if (step % 20 == 0):
-                feed = {x: mnist.test.images, y_: mnist.test.labels}
-                result = session.run([accuracy], feed_dict=feed)
-                summary_str = result[0]
-                print("Step %d: %2.3f" % (step, accuracy.eval(feed_dict=feed)*100) )
-            else:
-                batch = mnist.train.next_batch(batch_size)
-                session.run(optimizer, feed_dict={x: batch[0], y_: batch[1]})
